a7825aecf7fb88054091355c3db62b9910c55042
Tendai E. Mhona <mhonatendai@gmail.com>
2024-11-30 00:09:46 -0500

    first commit

---
+++ src/main/java/com/tenstech/employeeonboarding/EmployeeOnBoardingApplication.java
@@ -0,0 +1,13 @@
+package com.tenstech.employeeonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeOnBoardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeOnBoardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenstech/employeeonboarding/EmployeeOnBoardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenstech.employeeonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeOnBoardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
