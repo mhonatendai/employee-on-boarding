4ff923a80f355001c4e32585210fdb1f16410e0a
Tendai E. Mhona <mhonatendai@gmail.com>
2024-12-25 18:10:02 -0500

    Refactor

--- src/main/java/com/tenstech/employeeonboarding/costcenter/CostCenterRepository.java
+++ src/main/java/com/tenstech/employeeonboarding/costcenter/CostCenterRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CostCenterRepository extends JpaRepository<CostCenter, Long> {
-
-    Optional<CostCenter> findByName(String name);
 }
