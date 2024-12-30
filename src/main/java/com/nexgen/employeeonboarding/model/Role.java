dbea86c838720b63201747ecd0027da1ad196cb7
Tendai E. Mhona <mhonatendai@gmail.com>
2024-12-30 02:08:20 -0500

    Security config init.

---
+++ src/main/java/com/nexgen/employeeonboarding/model/Role.java
@@ -0,0 +1,19 @@
+package com.nexgen.employeeonboarding.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role extends AuditedEntity{
+
+    @Column
+    private String name;
+}
