ef9e29eb9f8fdc2b8c6ffc754e3ea802082b37a1
Tendai E. Mhona <mhonatendai@gmail.com>
2025-01-08 18:37:56 -0500

    Sec, refactor

--- src/main/java/com/nexgen/employeeonboarding/config/SecurityConfig.java
+++ src/main/java/com/nexgen/employeeonboarding/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.nexgen.employeeonboarding.config;
 
-import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,7 +12,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
